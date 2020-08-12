3640efcc3505d545a5c5391ac4662d86cd453078
Onur Okyay <o84169104@china.huawei.com>
2020-08-13 00:18:44 +0300

    UI - MyBooks Screen and Launcher icon were changed

--- app/src/screens/MyBookScreen.js
+++ app/src/screens/MyBookScreen.js
@@ -1,18 +1,76 @@
-import React from "react";
-import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+import React, { Component } from "react";
+import { AppRegistry, Text, StyleSheet, View, Button, TouchableOpacity, FlatList, Image } from "react-native";
 
-const MyBookScreen = ({ navigation }) => {
-  return (
-    <View>
-      <Text style={styles.text}>My Book List</Text>
-    </View>
-  );
-};
+
+export default class MyBooks extends Component {
+  constructor() {
+    super()
+    this.state = {
+      dataSource: []
+    }
+  }
+  renderItem = ({ item }) => {
+    return (
+      <View style={{ flex: 1, flexDirection: 'row',marginVertical:20}}>
+        <Image style={{ width: 100, height: 100 }}
+          source={{ uri: item.image }}
+          resizeMode='contain' />
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <Text style={{marginBottom:15}}>
+            {item.book_title}
+          </Text>
+          <Text>
+            {item.author}
+          </Text>
+        </View>
+      </View>
+    )
+  }
+  renderSeparator = ()=>{
+    return(
+      <View style={{height:1 ,width:'100%' ,backgroundColor:'black'}}>
+
+      </View>
+    );
+  }
+  componentDidMount() {
+    const url = 'http://www.json-generator.com/api/json/get/ceROvkrAEi?indent=2'
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          dataSource: responseJson.book_array
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          keyExtractor={book => book.book_title}
+          data={this.state.dataSource}
+          renderItem={this.renderItem}
+          ItemSeparatorComponent={this.renderSeparator}
+        />
+      </View>
+    );
+  }
+
+}
+
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 15
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
   }
 });
-
-export default MyBookScreen;
+<View>
+  <Text style={styles.text}>My Book List</Text>
+</View>
